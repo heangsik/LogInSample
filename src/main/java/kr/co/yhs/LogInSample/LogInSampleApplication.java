0f5318465b5ceefb5413a87597a08034413733d9
kkambook <ygun01@gmail.com>
2022-04-22 16:41:52 +0900

    first commit

---
+++ src/main/java/kr/co/yhs/LogInSample/LogInSampleApplication.java
@@ -0,0 +1,13 @@
+package kr.co.yhs.LogInSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogInSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogInSampleApplication.class, args);
+	}
+
+}
